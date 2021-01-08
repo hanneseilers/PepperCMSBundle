f307253c9781afa20e2e5ec99f983389812410b0
Hannes Eilers <mail@hanneseilers.de>
2021-01-08 09:15:38 +0100

    Updated

--- demo-apps/DemoMotionApp/app/src/main/java/de/fhkiel/pepper/demo/motion/MainActivity.java
+++ demo-apps/DemoMotionApp/app/src/main/java/de/fhkiel/pepper/demo/motion/MainActivity.java
@@ -17,7 +17,12 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
 
     private static final String TAG = MainActivity.class.getName();
 
+    private QiContext qiContext;
+
+    // Attributes to save animations
     private Animate animateScan;
+    private Animate animateDance;
+    private Animate animateMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +30,19 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
         setContentView(R.layout.activity_main);
         QiSDK.register(this, this);
 
+        // TODO: Connect buttons to motion function (see below)
         findViewById(R.id.btnScan).setOnClickListener(v -> {
-            new Thread(() -> {
-                Future<Void> animateFuture = animateScan.async().run();
-                animateScan.addOnStartedListener(() -> {
-                    Log.i( TAG, "Animation Scan started." );
-                });
-                animateFuture.thenConsume(voidFuture -> {
-                    if( voidFuture.isSuccess() ){
-                        Log.i(TAG, "Animation Scan successfull stopped.");
-                    } else if( voidFuture.hasError() ){
-                        Log.e(TAG, "Animation Scan finished with error!", voidFuture.getError() );
-                    }
-                });
-            }).start();
+            motion(animateScan);
         });
+
+        findViewById(R.id.btnDance).setOnClickListener(v -> {
+            motion(animateDance);
+        });
+
+        findViewById(R.id.btnMove).setOnClickListener(v -> {
+            motion(animateMove);
+        });
+
     }
 
     @Override
@@ -50,21 +53,68 @@ public class MainActivity extends RobotActivity implements RobotLifecycleCallbac
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
+        this.qiContext = qiContext;
+
+        // TODO: Create a animation from resource
         Animation animationScan = AnimationBuilder.with(qiContext)
                 .withResources(R.raw.animation_scan).build();
-        animateScan = AnimateBuilder.with(qiContext)
+        Animation animationDance = AnimationBuilder.with(qiContext)
+                .withResources(R.raw.dance_b003).build();
+        Animation animationMove = AnimationBuilder.with(qiContext)
+                .withResources(R.raw.trajectory_move).build();
+
+        // TODO: Build the animation ans save it to activity
+        this.animateScan = AnimateBuilder.with(qiContext)
                 .withAnimation(animationScan).build();
+        this.animateDance = AnimateBuilder.with(qiContext)
+                .withAnimation(animationDance).build();
+        this.animateMove = AnimateBuilder.with(qiContext)
+                .withAnimation(animationMove).build();
     }
 
     @Override
     public void onRobotFocusLost() {
+        this.qiContext = null;
+
+        // TODO: Remove listeners from animates
         if(animateScan != null){
             animateScan.removeAllOnStartedListeners();
+        }
+        if(animateDance != null){
+            animateDance.removeAllOnStartedListeners();
+        }
+        if(animateMove != null){
+            animateMove.removeAllOnStartedListeners();
         }
     }
 
     @Override
     public void onRobotFocusRefused(String reason) {
+        this.qiContext = null;
+    }
 
+    private void motion(Animate animate){
+        new Thread(() -> {
+
+            // Run the animate as asnc future
+            Future<Void> future = animate.async().run();
+
+            // Add a listener if animation starts
+            animate.addOnStartedListener(() -> {
+                Log.i( TAG, "Animation started." );
+            });
+
+            // Add listeners, if animation ends
+            future.thenConsume(voidFuture -> {
+
+                if( voidFuture.isSuccess() ){
+                    Log.i(TAG, "Animation successfull stopped.");
+                } else if( voidFuture.hasError() ){
+                    Log.e(TAG, "Animation finished with error!", voidFuture.getError() );
+                }
+
+            });
+
+        }).start();
     }
 }
