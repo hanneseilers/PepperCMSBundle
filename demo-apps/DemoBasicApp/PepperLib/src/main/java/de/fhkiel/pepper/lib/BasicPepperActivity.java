3b0d23838eb44353c80239b6923ebd1469af6461
Hannes Eilers <mail@hanneseilers.de>
2021-05-06 13:33:45 +0200

    Added BasicPepperActivity as base activity template.

---
+++ demo-apps/DemoBasicApp/PepperLib/src/main/java/de/fhkiel/pepper/lib/BasicPepperActivity.java
@@ -0,0 +1,72 @@
+package de.fhkiel.pepper.lib;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.design.activity.conversationstatus.SpeechBarDisplayStrategy;
+
+import org.json.JSONObject;
+
+/**
+ * Basic Activity to extend for own Pepper Applications
+ */
+public class BasicPepperActivity extends RobotActivity implements RobotLifecycleCallbacks, PepperLib.PepperLibCMSCallbackListener {
+
+    // Tag for logging
+    private static final String TAG = BasicPepperActivity.class.getName();
+
+    // Reference to Pepper Library
+    public PepperLib pepperLib;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // hide big speech bar
+        setSpeechBarDisplayStrategy( SpeechBarDisplayStrategy.IMMERSIVE );
+
+        // Register Robot SDK
+        QiSDK.register(this, this);
+
+        // Create PepperLib object
+        this.pepperLib = new PepperLib(this);
+
+        // Process intent data from CMS
+        this.pepperLib.processCMSIntent(getIntent());
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        this.pepperLib.onRobotFocusGained(qiContext);
+        Log.d(TAG, "Robot focus gained.");
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        this.pepperLib.onRobotFocusLost();
+        Log.w(TAG, "Robot focus lost.");
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        this.pepperLib.onRobotFocusRefused(reason);
+        Log.e(TAG, "Robot focus refused!");
+    }
+
+    @Override
+    public void onCMSIntentProcessed(JSONObject appData, JSONObject userData, JSONObject payloadDdata) {
+        // Intent data is ready
+        Log.i(TAG, "Processed CMS Intent.");
+    }
+}
