096cda2da84b772eec0a435862077aa7a4080e2f
Hannes Eilers <mail@hanneseilers.de>
2021-05-06 13:34:07 +0200

    Updated to basic activity template form lib.

--- demo-apps/DemoBasicApp/basic/src/main/java/de/fhkiel/pepper/cms/demo/basic/BasicActivity.java
+++ demo-apps/DemoBasicApp/basic/src/main/java/de/fhkiel/pepper/cms/demo/basic/BasicActivity.java
@@ -16,45 +16,32 @@ import org.json.JSONObject;
 
 import java.util.Date;
 
+import de.fhkiel.pepper.lib.BasicPepperActivity;
 import de.fhkiel.pepper.lib.PepperLib;
 
-public class BasicActivity extends RobotActivity implements RobotLifecycleCallbacks, PepperLib.PepperLibCMSCallbackListener {
+public class BasicActivity extends BasicPepperActivity {
 
     // Tag for logging
     private static final String TAG = BasicActivity.class.getName();
-
-    // Reference to Pepper Library
-    public PepperLib pepperLib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // hide big speech bar
-        setSpeechBarDisplayStrategy( SpeechBarDisplayStrategy.IMMERSIVE );
-
-        // Register Robot SDK
-        QiSDK.register(this, this);
-
-        // Create PepperLib object
-        this.pepperLib = new PepperLib(this);
-
-        // Process intent data from CMS
-        this.pepperLib.processCMSIntent(getIntent());
-
         // close app on button click and set timestamp to return data
         findViewById(R.id.btnClose).setOnClickListener(v -> {
             // Demo setting intent result to return to CMS
             // Data is tored in CMS with the user currently logged in and passed back
             // as Intent data for this user on next App start
-            /*try {
+            try {
                 JSONObject payloadData = new JSONObject();
                 payloadData.put( "timestamp", (new Date()).getTime() );
+                this.pepperLib.setPayloadData(payloadData);
                 Log.d( TAG, "set timestamp as result");
             } catch (JSONException e){
                 Log.w( TAG, e);
-            }*/
+            }
 
             closeApp();
         });
@@ -70,33 +57,4 @@ public class BasicActivity extends RobotActivity implements RobotLifecycleCallba
         this.finishAndRemoveTask();
     }
 
-    @Override
-    protected void onDestroy() {
-        QiSDK.unregister(this, this);
-        super.onDestroy();
-    }
-
-    @Override
-    public void onRobotFocusGained(QiContext qiContext) {
-        this.pepperLib.onRobotFocusGained(qiContext);
-        Log.d(TAG, "Robot focus gained.");
-    }
-
-    @Override
-    public void onRobotFocusLost() {
-        this.pepperLib.onRobotFocusLost();
-        Log.w(TAG, "Robot focus lost.");
-    }
-
-    @Override
-    public void onRobotFocusRefused(String reason) {
-        this.pepperLib.onRobotFocusRefused(reason);
-        Log.e(TAG, "Robot focus refused!");
-    }
-
-    @Override
-    public void onCMSIntentProcessed(JSONObject appData, JSONObject userData, JSONObject payloadDdata) {
-        // Intent data is ready
-        Log.i(TAG, "Processed CMS Intent.");
-    }
 }
