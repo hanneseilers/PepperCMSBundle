fc57f77abcca3df5142ed307214e2391f318657e
Hannes Eilers <mail@hanneseilers.de>
2021-05-06 17:24:50 +0200

    Optimized imports

--- demo-apps/DemoBasicApp/speech/src/test/java/de/fhkiel/pepper/cms/demo/speech/ExampleUnitTest.java
+++ demo-apps/DemoBasicApp/speech/src/test/java/de/fhkiel/pepper/cms/demo/speech/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package de.fhkiel.pepper.cms.demo.speech;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
